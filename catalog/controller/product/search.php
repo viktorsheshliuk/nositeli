406d7ca518dafcf2ec66e890f43e03e201b8564a
Viktor Sheshliuk <viktor.sheshliuk@artilab.pro>
2026-08-15 23:38:41 +0300

    product thumb

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -202,62 +202,6 @@ class ControllerProductSearch extends Controller {
                 'products' => $results
             ]);
 
-			
-
-
-
-
-
-
-
-
-			foreach ($results as $result) {
-				if ($result['image']) {
-					$image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-				} else {
-					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
-				}
-
-				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
-					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-				} else {
-					$price = false;
-				}
-
-				if ((float)$result['special']) {
-					$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
-				} else {
-					$special = false;
-				}
-
-				if ($this->config->get('config_tax')) {
-					$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
-				} else {
-					$tax = false;
-				}
-
-				if ($this->config->get('config_review_status')) {
-					$rating = (int)$result['rating'];
-				} else {
-					$rating = false;
-				}
-
-				// $data['products'][] = array(
-				// 	'stock_status_id' => $result['stock_status_id'],
-			    //     'ostatok'     => $result['quantity'],
-				// 	'product_id'  => $result['product_id'],
-				// 	'thumb'       => $image,
-				// 	'name'        => $result['name'],
-				// 	'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
-				// 	'price'       => $price,
-				// 	'special'     => $special,
-				// 	'tax'         => $tax,
-				// 	'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-				// 	'rating'      => $result['rating'],
-				// 	'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url)
-				// );
-			}
-
 			$url = '';
 
 			if (isset($this->request->get['search'])) {
